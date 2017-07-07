63ff5af6f8c2890bd749de091b1984017acebae6
Badichan Polina <badichan@mail.ru>
2017-07-07 16:48:34 +0200

    lesson-27

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -5,13 +5,7 @@ class AnswersController < ApplicationController
   before_action :set_answer, only: [:destroy]
 
   def create
-    @answer = @question.answers.new(answer_params)
-    @answer.user = current_user
-    if @answer.save
-      redirect_to @question, notice: 'Ваш ответ успешно добален'
-    else
-      render 'questions/show'
-    end
+    @answer = @question.answers.create(answer_params.merge(user: current_user))
   end
 
   def destroy
