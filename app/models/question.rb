78996d86964305ac164db0ec36d604475855ed52
Badichan Polina <badichan@mail.ru>
2017-07-26 19:32:26 +0200

    Attachment model

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,7 @@ class Question < ApplicationRecord
 
   has_many :answers, dependent: :destroy
   belongs_to :user
+  has_many :attachments
 
   validates :title, :body, presence: true
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170717093333) do
+ActiveRecord::Schema.define(version: 20170726153310) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 20170717093333) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+  end
+
+  create_table "attachments", force: :cascade do |t|
+    t.string "file"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "question_id"
+    t.index ["question_id"], name: "index_attachments_on_question_id"
   end
 
   create_table "questions", force: :cascade do |t|
