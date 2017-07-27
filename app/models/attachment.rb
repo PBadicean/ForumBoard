32ffbcacaac96036bca78b7ede7c15ac8e179067
Badichan Polina <badichan@mail.ru>
2017-07-27 12:53:49 +0200

    User can add files to answers

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,6 +1,6 @@
 class Attachment < ApplicationRecord
 
-  belongs_to :attachmentable, polymorphic: true
+  belongs_to :attachmentable, polymorphic: true, optional: true
   mount_uploader :file, FileUploader
 
 end
--- spec/acceptance/answers/add_files_to_answer_spec.rb
+++ spec/acceptance/answers/add_files_to_answer_spec.rb
@@ -14,7 +14,7 @@ feature 'Add files to answer', '
     visit question_path(question)
   end
 
-  scenario 'User adds file when asks question' do
+  scenario 'User adds file when keep answer', js: true do
     fill_in 'Ответ', with: 'труляля'
     attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
     click_on 'Сохранить'
