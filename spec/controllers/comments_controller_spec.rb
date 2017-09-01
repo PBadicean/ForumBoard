ee722362d78fc1d3b2aa3d23b8f41a6017da0205
Badichan Polina <badichan@mail.ru>
2017-09-01 17:22:05 +0200

    Comments controller with spec

---
+++ spec/controllers/comments_controller_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe CommentsController, type: :controller do
+  sign_in_user
+  let(:question) { create(:question) }
+  let(:comment)  { create(:comment, commentable: question) }
+
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'creates a new comment' do
+        expect do
+          post :create, params: { question_id: question,
+            comment: attributes_for(:comment),
+            format: :json }
+        end.to change(question.comments, :count).by(1)
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'does not create a new comment' do
+        expect do
+          post :create, params: { question_id: question,
+            comment: attributes_for(:invalid_comment),
+            format: :json }
+        end.to_not change(question.comments, :count)
+      end
+    end
+  end
+end
