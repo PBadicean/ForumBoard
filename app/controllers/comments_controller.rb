674aa010dd659cf3b2e5e2b5b3c69858d5570f09
Badichan Polina <badichan@mail.ru>
2017-10-04 17:17:45 +0200

    lesson - 34

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,7 @@
 class CommentsController < ApplicationController
 
+  authorize_resource
+
   before_action :ensure_signup_complete
   before_action :set_commentable
   after_action :publish_comment
--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -20,8 +20,18 @@ describe Ability do
   end
 
   describe 'for user' do
-    let(:user) { create :user }
-    let(:other) { create :user }
+    let(:user)     { create :user }
+    let(:other)    { create :user }
+
+    let(:best_answer)       { create :answer }
+    let(:question_with_best){ create :question, best_answer: best_answer.id }
+    let(:answer_own_user)   { create :answer, question: (create :question, user: user) }
+    let(:answer_other_user) { create :answer, question: (create :question, user: other) }
+    let(:vote_question)     { create(:vote, votable: create(:question, user: other), user: user) }
+    let(:vote_answer)       { create(:vote, votable: create(:answer, user: other), user: user) }
+
+    let(:attachment_own_user)   { create :attachment, attachable: (create :question, user: user) }
+    let(:attachment_other_user) { create :attachment, attachable: (create :question, user: other) }
 
     it { should_not be_able_to :manage, :all }
     it { should be_able_to :read, :all }
@@ -41,5 +51,30 @@ describe Ability do
 
     it { should be_able_to :destroy, (create :answer, user: user), user: user }
     it { should_not be_able_to :destroy, (create :answer, user: other), user: user }
+
+    it { should be_able_to :up_vote, (create :question, user: other), user: user }
+    it { should_not be_able_to :up_vote, (create :question, user: user), user: user }
+
+    it { should be_able_to :down_vote, (create :question, user: other), user: user }
+    it { should_not be_able_to :down_vote, (create :question, user: user), user: user }
+
+    it { should be_able_to :revote, vote_question.votable, user: user }
+    it { should_not be_able_to :revote, (create :question, user: other), user: user }
+
+    it { should be_able_to :up_vote, (create :answer, user: other), user: user }
+    it { should_not be_able_to :up_vote, (create :answer, user: user), user: user }
+
+    it { should be_able_to :down_vote, (create :answer, user: other), user: user }
+    it { should_not be_able_to :down_vote, (create :answer, user: user), user: user }
+
+    it { should be_able_to :revote, vote_answer.votable, user: user }
+    it { should_not be_able_to :revote, (create :answer, user: other), user: user }
+
+    it { should be_able_to :accept, answer_own_user, user: user }
+    it { should_not be_able_to :accept, answer_other_user, user: user }
+    it { should_not be_able_to :accept, best_answer, user: user }
+
+    it { should be_able_to :destroy, attachment_own_user, user: user }
+    it { should_not be_able_to :destroy, attachment_other_user, user: user }
   end
 end
