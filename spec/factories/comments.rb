6502a052e97a24c345752e2dd5b321da921f3e81
Badichan Polina <badichan@mail.ru>
2017-10-10 12:09:15 +0200

    Add spec comments to question-show with API

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -2,6 +2,7 @@ FactoryGirl.define do
   factory :comment do
     sequence(:body){ |n| "Mycomment#{n}" }
     association :commentable
+    user
      factory :invalid_comment do
        body nil
        association :commentable
