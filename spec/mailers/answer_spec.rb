09f5232b0a597b0631d3393f37ccb165037fefa4
Badichan Polina <badichan@mail.ru>
2017-10-25 22:36:45 +0200

    test method new_answer of NotifyDigestJob

--- spec/mailers/answer_spec.rb
+++ spec/mailers/answer_spec.rb
@@ -2,8 +2,7 @@ require "rails_helper"
 
 RSpec.describe AnswerMailer, type: :mailer do
   let(:user)   { create(:user) }
-  let(:question) { create(:question, user: user) }
-  let(:answer)   { create(:answer, question: question) }
+  let(:answer)   { create(:answer) }
   let(:mail) { AnswerMailer.new_answer(answer, user) }
 
   it 'renders the body' do
