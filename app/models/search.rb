8dce7e8411470f132bd0b5a4724ed711103b000e
Badichan Polina <badichan@mail.ru>
2017-11-06 22:52:04 +0100

    lesson-39

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,7 +1,8 @@
 class Search
   DIVISIONS = %w(Everywhere Question Answer Comment User)
 
-  def self.find(query, division)
+  def self.find_object(query, division)
+    query = Riddle::Query.escape(query)
     return ThinkingSphinx.search query if division == 'Everywhere'
     division.classify.constantize.search(query)
   end
