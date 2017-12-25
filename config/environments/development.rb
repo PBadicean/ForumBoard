49a7386163c5c1e6093052976a891ac62b3574ce
Badichan Polina <badichan@mail.ru>
2017-12-25 19:43:08 +0100

    lesson-43

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.perform_caching = false
+  config.action_mailer.perform_caching = true
 
   config.active_support.deprecation = :log
 
