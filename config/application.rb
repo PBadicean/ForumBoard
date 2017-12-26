b70db8c35f8ffc6958f17854ecce681bc6ec9cfc
Badichan Polina <badichan@mail.ru>
2017-12-26 21:31:51 +0100

    Caching in model

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,8 @@ module Qna
                         request_specs: false,
                         controller_spec: true
       g.fixtures_replacement :factory_girl, dir: 'spec/factories'
+
+      config.cache_store = :redis_store, 'redis://localhost:6379/0/cache', { expires_in: 90.minutes }
     end
   end
 end
